9ae34e7f4876fcc7c7a026660ac8be440c7dcfde
brundhask <brundhasubramanian@gmail.com>
2021-10-27 21:42:30 -0700

    alert message added

--- script.js
+++ script.js
@@ -2,24 +2,30 @@ function calculatebmi() {
   var weight = document.getElementById("weight").value;
   var height = document.getElementById("height").value;
 
-  height *= 30.48;
-  height /= 10;
-
-  var bmivalue = weight / (height * height);
-  bmivalue *= 100;
-  bmivalue = bmivalue.toFixed(1);
-
-  if (bmivalue < 18.5) {
-    document.getElementById("card-ans").innerHTML =
-      bmivalue + " kg/m <sup>2</sup>" + "<br />" + "(Underweight)";
-  } else if (bmivalue > 24.9 && bmivalue < 30) {
-    document.getElementById("card-ans").innerHTML =
-      bmivalue + " kg/m <sup>2</sup>" + "<br />" + "(Overweight)";
-  } else if (bmivalue > 30.0) {
-    document.getElementById("card-ans").innerHTML =
-      bmivalue + " kg/m <sup>2</sup>" + "<br />" + "(Obesity)";
+  if (weight === "" || weight === null) {
+    alert("Enter Weight to calculate..");
+  } else if (height === "" || height === null) {
+    alert("Enter Height to calculate..");
   } else {
-    document.getElementById("card-ans").innerHTML =
-      bmivalue + " kg/m <sup>2</sup>" + "<br />" + "(Normal)";
+    height *= 30.48;
+    height /= 10;
+
+    var bmivalue = weight / (height * height);
+    bmivalue *= 100;
+    bmivalue = bmivalue.toFixed(1);
+
+    if (bmivalue < 18.5) {
+      document.getElementById("card-ans").innerHTML =
+        bmivalue + " kg/m <sup>2</sup>" + "<br />" + "(Underweight)";
+    } else if (bmivalue > 24.9 && bmivalue < 30) {
+      document.getElementById("card-ans").innerHTML =
+        bmivalue + " kg/m <sup>2</sup>" + "<br />" + "(Overweight)";
+    } else if (bmivalue > 30.0) {
+      document.getElementById("card-ans").innerHTML =
+        bmivalue + " kg/m <sup>2</sup>" + "<br />" + "(Obesity)";
+    } else {
+      document.getElementById("card-ans").innerHTML =
+        bmivalue + " kg/m <sup>2</sup>" + "<br />" + "(Normal)";
+    }
   }
 }
